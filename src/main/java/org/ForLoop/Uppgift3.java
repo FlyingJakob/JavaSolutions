f44b2aafe43478b19502791941cf0e042677a0cd
Jakob Forsbacka <jakob.forsbacka@gmail.com>
2023-10-17 12:38:16 +0200

    For loop

---
+++ src/main/java/org/ForLoop/Uppgift3.java
@@ -0,0 +1,38 @@
+package org.ForLoop;
+
+public class Uppgift3 {
+    public static void main(String[] args) {
+
+        String original = "Apa";
+
+        original = original.toLowerCase();
+
+
+        String reversed = "";
+
+
+        StringBuilder output = new StringBuilder(original).reverse();
+
+        reversed = output.toString();
+        System.out.println(reversed);
+        System.out.println(original);
+
+        /*
+        for (int i = original.length()-1; i >= 0 ; i--) {
+            reversed+=original.charAt(i);
+        }
+        */
+
+
+
+        if (original.equals(reversed)){
+            System.out.println("Det var ett palindrom");
+        }else{
+            System.out.println("Det var inte ett palindrom");
+        }
+
+
+
+
+    }
+}
