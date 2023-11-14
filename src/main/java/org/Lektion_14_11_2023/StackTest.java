13c2f843845eb163d50c458660b75236c12fcf52
Jakob Forsbacka <jakob.forsbacka@gmail.com>
2023-11-14 14:47:00 +0100

    test

---
+++ src/main/java/org/Lektion_14_11_2023/StackTest.java
@@ -0,0 +1,30 @@
+package org.Lektion_14_11_2023;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StackTest {
+
+    @Test
+    public void TestStack(){
+        Stack stack = new Stack();
+
+        stack.push("Jakob");
+
+        assertEquals("Jakob",stack.pop());
+
+        stack.push("Kalle");
+        stack.push("Lisa");
+
+        assertEquals("Lisa",stack.pop());
+        assertEquals("Kalle",stack.pop());
+
+        assertEquals(null,stack.pop());
+
+
+
+
+    }
+
+}
